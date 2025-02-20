deb9c4f9891748873a4b6541c7e0b04fc151d34e
Adam <adam@haglund.dev>
2025-02-20 13:40:04 +0100

    up deps

--- eslint.config.js
+++ eslint.config.js
@@ -20,6 +20,7 @@ export default antfu({
       "ts/no-unsafe-assignment": "off",
       "node/prefer-global/process": "off",
       "antfu/no-top-level-await": "off",
+      "import/consistent-type-specifier-style": "off",
 
       "perfectionist/sort-imports": [
         "error",
