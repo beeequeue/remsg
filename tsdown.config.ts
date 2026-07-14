10fc9dd35c38d8101a2f609bad9370752cf6f6da
bq <adam@haglund.dev>
2026-07-14 11:28:24 +0200

    improve bundled paths

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,13 +1,47 @@
+import path from "node:path"
+
 import { defineConfig } from "tsdown"
+import type { NodeProtocolPlugin } from "tsdown/plugins"
+
+export const vendoredPathsPlugin = () =>
+  ({
+    name: "vendored-paths",
+    outputOptions(config) {
+      const original = config.entryFileNames as (i: unknown) => string
+
+      config.entryFileNames = (i) => {
+        const modulePath = i.facadeModuleId!
+
+        if (modulePath.endsWith("package.json") && !modulePath.includes("node_modules")) {
+          return "manifest.mjs"
+        }
+
+        if (i.name.startsWith("node_modules")) {
+          const matches = /\.pnpm[\\/](.+?@.+?)[\\/]/.exec(modulePath)
+          if (matches != null && matches[1] != null) {
+            const name = matches[1].split("@")[0]!
+            const endOfPath = i.facadeModuleId!.slice(
+              i.facadeModuleId!.indexOf(matches[1]) + matches[1].length,
+            )
+            return path.join("_vendor", matches[1], endOfPath.split(name).slice(1)!.join(name))
+          }
+
+          return path.join("_vendor", "unknown.mjs")
+        }
+
+        return original(i)
+      }
+    },
+  }) satisfies ReturnType<typeof NodeProtocolPlugin>
 
 export default defineConfig({
   entry: ["src/index.ts"],
   outDir: "dist",
   unbundle: true,
+  // removes jsdoc comments from JS output, keeps them in TS
+  outputOptions: { comments: { jsdoc: false } },
   deps: { onlyBundle: ["murmur-hash"] },
-  outputOptions: {
-    comments: { jsdoc: false }, // removes jsdoc comments from JS output, keeps them in TS
-  },
+  exports: true,
 
   env: { TEST: false },
 
@@ -16,4 +50,6 @@ export default defineConfig({
   dts: { oxc: true },
   fixedExtension: true,
   minify: "dce-only",
+
+  plugins: [vendoredPathsPlugin()],
 })
